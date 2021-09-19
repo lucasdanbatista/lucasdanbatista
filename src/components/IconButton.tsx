dfe99157454683c742e2a9561a81209c1694f20f
Lucas Batista <contact@lucasbatista.me>
2021-09-19 13:24:55 -0300

    Updates social medias

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -24,14 +24,14 @@ export default class IconButton extends React.Component<IconButtonProps, IconBut
         return (
             <button
                 style={{
+                    width: '7vh',
+                    height: '7vh',
                     borderRadius: '100%',
-                    width: '6vh',
-                    height: '6vh',
                     backgroundColor: `rgba(255, 255, 255, ${this.state.hovered ? 0.5 : 0.8})`,
                     border: 'none',
                     color: 'rgba(0, 0, 0, 0.8)',
                     margin: '8px',
-                    cursor: 'pointer'
+                    cursor: 'pointer',
                 }}
                 onMouseOver={() => this.setState({hovered: true})}
                 onMouseOut={() => this.setState({hovered: false})}
